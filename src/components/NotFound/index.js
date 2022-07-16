002ea7d4328dc7d12fc56d47f4ecc4e655de7017
modepalli chaitanya <94157492+modepallichaitanya@users.noreply.github.com>
2022-07-16 11:20:27 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,16 @@
 // Write your JS code here
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png "
+      alt="home"
+      className="not-found-img"
+    />
+    <h1 className="not-found-heading">NotFound</h1>
+  </div>
+)
+
+export default NotFound
